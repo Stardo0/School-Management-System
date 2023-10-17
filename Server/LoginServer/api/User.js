02c3b808d036bb751b00024129d4cc7d1246022e
Stardo <marc.blattmann08@gmail.com>
2023-10-17 17:24:52 +0200

    Login System Backend with Express and MongoDB

---
+++ Server/LoginServer/api/User.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+// Signup
+router.post('/singnup', (req, res) => {
+
+})
+
+// Singin
+router.post('/signin', (req, res) => {
+
+})
+
+module.exports = router;
